b3722ab8770d2b9a6fcce0a9a82e4c11f89fff44
le4013614 <32262259-le4013614@users.noreply.replit.com>
2024-03-06 13:52:40 +0000

    codigos da aula2 e da pratica1
    w

---
+++ .ccls-cache/@home@runner@apc1-engdm1a/Praticas@pratica01@questao02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()  {
+int base = 2;
+  int altura = 3;
+
+  int area = base*altura / 2;
+  
+  return 0;
+}
+
+
---
+++ Praticas/pratica01/questao01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()  {
+float nota1 = 5.5f;
+  float nota2 =4.0f;
+
+  float media =0.4* nota1 + 0.6*nota2;
+  printf("A média é %.2f\n", media);
+  
+  return 0;
+}
